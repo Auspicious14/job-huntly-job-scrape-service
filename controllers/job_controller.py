e0474477292873bf2cb0dd8bc76828bceefd59c6
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-06-23 22:45:46 +0100

    refactor: remove /jobs prefix

--- controllers/job_controller.py
+++ controllers/job_controller.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter
 from models.job_models import Payload, JobResponse
 from services.scraper_service import scrape_jobs
 
-router = APIRouter(prefix="/jobs", tags=["jobs"])
+# router = APIRouter(prefix="/jobs", tags=["jobs"])
 
-@router.post("/scrape", response_model=List[JobResponse])
+@router.post("/jobs/scrape", response_model=List[JobResponse])
 async def scrape_jobs_controller(payload: Payload):
     """
     Controller handling job scraping requests
