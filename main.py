5062e2b13a26702a802cf470615f6fce14986606
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-06-23 14:59:28 +0100

    feat: add CORS support and simplify vercel config

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
-from .routes.job_routes import router
+from routes.job_routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
