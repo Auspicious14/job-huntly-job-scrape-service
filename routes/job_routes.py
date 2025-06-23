bcf413e50b8b6b986671cb7b5a918a76f75d9487
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-06-23 15:05:53 +0100

    refactor: update API endpoint prefix from /api/v1 to /api

--- routes/job_routes.py
+++ routes/job_routes.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from controllers.job_controller import router
 
 router = APIRouter()
-router.include_router(router, prefix="/api/v1")
+router.include_router(router, prefix="/api")
